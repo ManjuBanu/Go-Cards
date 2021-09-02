5849e0c18fadf1f8655a07e63d811c15ccbfb9cc
v484407 <manjumala.devim@amerisourcebergen.com>
2021-09-02 20:58:39 +0530

    First commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//variable declaration
+	//[type 1]
+	var card string = "Ace of Spade"
+
+	//[type 2]
+	cards := "Ace"
+
+	fmt.Println(card)
+	fmt.Println("\n", cards)
+}
